31ea7dcaa30baa042a55dc28312b9d4a6da90d12
Javi Roman <jroman.espinar@gmail.com>
2014-05-16 23:10:57 +0200

    deploop.rb fixed

--- aritfacts/mco-agents/deploop/mcollective-deploop-agent-1.0.0/usr/libexec/mcollective/mcollective/agent/deploop.rb
+++ aritfacts/mco-agents/deploop/mcollective-deploop-agent-1.0.0/usr/libexec/mcollective/mcollective/agent/deploop.rb
@@ -1,20 +1,20 @@
 # vim: autoindent tabstop=2 shiftwidth=2 expandtab softtabstop=2 filetype=ruby
-
 module MCollective
   module Agent
     class Deploop<RPC::Agent
 
-      action "download_fact" do
+      action 'download_fact' do
         validate :url, String
 
         url = request[:url]
         Log.info("url passed: %s" % url)
 
+        require 'rubygems'
         require 'curb'
-	require 'facter'
-    	require 'yaml'
+	      require 'facter'
+    	  require 'yaml'
 
-        facterpath='/var/lib/puppet/lib/facter'
+        facterpath='/var/lib/puppet/facts.d'
 
         filename=url.split('/').last
         fileout = "#{facterpath}/#{filename}"
@@ -24,25 +24,25 @@ module MCollective
 
         config = MCollective::Config.instance
 
-	yamlfile = config.pluginconf["yaml"]
+	      yamlfile = config.pluginconf["yaml"]
         Log.info("YAML file to update: %s" % yamlfile)
 
-	rejected_facts = ["sshdsakey", "sshrsakey"]
+	      rejected_facts = ['sshdsakey', 'sshrsakey']
 
-	Facter.clear
-	Facter.search(facterpath)
-	Facter.loadfacts
-	Facter.flush
+	      Facter.clear
+	      Facter.search(facterpath)
+	      Facter.loadfacts
+	      Facter.flush
 	
-	facts = Facter.to_hash.reject { |k,v| rejected_facts.include? k }
+	      facts = Facter.to_hash.reject { |k,v| rejected_facts.include? k }
 
-	File.open(yamlfile, "w") { |fh| 
-	  fh.write facts.to_yaml
-	  fh.close
+	      File.open(yamlfile, "w") { |fh| 
+	        fh.write facts.to_yaml
+	        fh.close
         }
 
-        reply["response_code"] = c.response_code 
-        reply["downloaded_content_length"] = c.downloaded_content_length 
+        reply['response_code'] = c.response_code 
+        reply['downloaded_content_length'] = c.downloaded_content_length 
       end
     end
   end
