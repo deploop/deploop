d71d483ac458c0a8bb6e5deedcfe8d49fb187728
Javi Roman <jroman.espinar@gmail.com>
2014-05-19 23:04:39 +0200

    puppet environments initial schema

--- scripts/mco_deploop.sh
+++ scripts/mco_deploop.sh
@@ -2,19 +2,29 @@
 # [1] http://docs.puppetlabs.com/mcollective/reference/plugins/facts.html
 # [2] https://github.com/puppetlabs/marionette-collective/tree/master/plugins/mcollective/facts
 
-#DEPLOOP_FACT="deploop_role.rb"
-#DEPLOOP_FACT="deploop_category.rb"
-#DEPLOOP_FACT="deploop_collection.rb"
-DEPLOOP_FACT="deploop_entity.rb"
-
-# mco rpc deploop puppet_environment env=production --with-identity=mncars001
-# mco rpc deploop puppet_environment env=preproduction
-# mco rpc deploop puppet_environment env=test
-
+#
+# 1. The fist step is define the Puppet environmet per host:
 # This match with the Puppet environment configuration
 # /etc/puppet/environments/production
 #                          preproduction
 #                          test
+# mco rpc deploop puppet_environment env=production --with-identity=mncars001
+# mco rpc deploop puppet_environment env=preproduction --with-identity=mncars001
+# mco rpc deploop puppet_environment env=test
+
+#
+# 2. For test the properly enviroment setup we have to run on each node:
+#
+# puppet agent --configprint environment
+
+#
+# 3. The next one is deploy de facts for fine tune logic control in puppet master.
+#
+
+#DEPLOOP_FACT="deploop_role.rb"
+#DEPLOOP_FACT="deploop_category.rb"
+#DEPLOOP_FACT="deploop_collection.rb"
+DEPLOOP_FACT="deploop_entity.rb"
 
 mco rpc deploop download_fact url="http://openbus-deploop:8080/$DEPLOOP_FACT"
 
