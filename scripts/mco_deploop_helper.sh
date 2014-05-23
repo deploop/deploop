da0722cd2c850ee62eb4c757c21cb172bb9c8a71
Javi Roman <javiroman@redoop.org>
2014-05-23 13:34:21 +0200

    helper script

--- scripts/mco_deploop_helper.sh
+++ scripts/mco_deploop_helper.sh
@@ -1,45 +1,83 @@
-# Example for copy new facts in server node. We have to take into account [1] and [2]
-# [1] http://docs.puppetlabs.com/mcollective/reference/plugins/facts.html
-# [2] https://github.com/puppetlabs/marionette-collective/tree/master/plugins/mcollective/facts
+# This script is only for development. It's a dummy guide in order
+# to deploy a Deploop enabled Lambda Architecture cluster.
 
-#
+exit 1
+
+###################################################################
 # 1. The fist step is define the Puppet environmet per host:
+#
 # This match with the Puppet environment configuration
 # /etc/puppet/environments/production
 #                          preproduction
 #                          test
-# mco rpc deploop puppet_environment env=production --with-identity=mncars001
-# mco rpc deploop puppet_environment env=preproduction --with-identity=mncars001
-# mco rpc deploop puppet_environment env=test
+#
+# mco rpc <deploop agent> <action> env=<enviroment> 
+#
+# This command insert on each /etc/puppet/puppet.conf node:
+#
+#  pluginsync=false  [deploop issue #2]
+#  factpath=/var/lib/puppet/facts.d/ [facts location for puppet]
+#  environment=production [built-in environment mechanism for puppet]
+###################################################################
+
+manager> mco find --with-agent deploop # for sanity checking
+manager> mco rpc deploop puppet_environment env=production --with-identity=mncars001
+manager> mco rpc deploop puppet_environment env=preproduction --with-identity=mncars001
+manager> mco rpc deploop puppet_environment env=test
+
+###################################################################
+# 2. For testing the properly enviroment setup we have to run on each node:
+###################################################################
+
+agent> puppet agent --configprint environment
+
+###################################################################
+# 3. The next one is to deploy de facts for fine tune logic control in puppet master.
+#
+# In order to makes work this step, we have to run a simple webserver
+# pointing to the deploop facts folder
+###################################################################
+
+manager> setsid ruby ./simple-webserver.rb
+
+# deploop_collection.rb -> production
+# deploop_category.rb -> batch
+# deploop_role.rb -> nn1
+# deploop_entity.rb -> flume
 
 #
-# 2. For test the properly enviroment setup we have to run on each node:
+# mco rpc <deploop agent> <action> url=<enviroment> 
+# 
+# This command download the deploop fact into the agent folder /var/lib/puppet/facts.d
+# and update the facters file: /etc/mcollective/facts.yaml
 #
-# puppet agent --configprint environment
+manager> mco rpc deploop download_fact url="http://openbus-deploop:8080/deploop_collection.rb" --with-identity=mncars001
+manager> mco rpc deploop download_fact url="http://openbus-deploop:8080/deploop_category.rb" --with-identity=mncars001
+manager> mco rpc deploop download_fact url="http://openbus-deploop:8080/deploop_role.rb" --with-identity=mncars001
+manager> mco rpc deploop download_fact url="http://openbus-deploop:8080/deploop_entity.rb" --with-identity=mncars001
 
-#
-# 3. The next one is deploy de facts for fine tune logic control in puppet master.
-#
-
-#DEPLOOP_FACT="deploop_role.rb"
-#DEPLOOP_FACT="deploop_category.rb"
-#DEPLOOP_FACT="deploop_collection.rb"
-DEPLOOP_FACT="deploop_entity.rb"
-
-mco rpc deploop download_fact url="http://openbus-deploop:8080/$DEPLOOP_FACT"
-
-# The file must be go to /var/lib/puppet/lib/facter/ and update with 
-# facter -p -y > /etc/mcollective/facts.yaml
-
-
+###################################################################
+# 4. Getting information
 # Check for the new Deploop Fact:
+###################################################################
 
-mco ping --with-fact "${DEPLOOP_FACT%.rb}=/.*/"
-mco inventory openbus-nn1 | grep deploop | grep =
+manager> mco rpc rpcutil get_fact fact=deploop_collection
+manager> mco rpc rpcutil get_fact fact=deploop_collection
+manager> mco ping --with-fact "deploop_collection=/.*/"
+manager> mco ping --with-fact "deploop_category=/.*/"
+manager> mco inventory mncars001 | grep deploop_
+manager> mco plugin doc agent/deploop
+manager> mco plugin doc agent/service
+manager> mco plugin doc agent/puppet
 
+###################################################################
+# 5. Schedule Puppet runs for real deployment:
+###################################################################
 
-# Puppet runs
-# mco rpc puppet runonce --with-identity mncars001
+manager> mco rpc puppet runonce 
+manager> mco rpc puppet runonce --with-identity mncars001
+manager> mco rpc puppet runonce --batch 2 --batch-sleep 10
+manager> mco rpc puppet status --with-identity=mncars001
 
 
 
